b8fc0135726452d678a4db4ff667ef296b2131df
Louis Hong <www.louishong.com@me.com>
2013-05-21 00:24:19 +0800

    Chaged the way to creat xml from Format Strings to using Stax XMLOutputFactory

--- src/com/louishong/ibg/weixin/WeixinServer.java
+++ src/com/louishong/ibg/weixin/WeixinServer.java
@@ -11,9 +11,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 
+import javax.xml.stream.XMLEventFactory;
 import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLEventWriter;
 import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Characters;
+import javax.xml.stream.events.EndDocument;
+import javax.xml.stream.events.EndElement;
+import javax.xml.stream.events.StartDocument;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 
@@ -67,7 +74,7 @@ public class WeixinServer extends HttpServlet {
 		try {
 			PrintWriter out = response.getWriter();
 			System.out.println("Connection Success");
-			out.print(respondRequest(request));
+			respondRequest(request, out);
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
@@ -160,23 +167,116 @@ public class WeixinServer extends HttpServlet {
 		return null;
 	}
 
-	protected String respondRequest(HttpServletRequest request) {
+	protected void respondRequest(HttpServletRequest request, PrintWriter out) {
 		WeixinMessage message = readRequest(request);
-		if (((Text) message).getMsgType().equals(WeixinMessage.TEXT)) {
-			String response = String.format("<xml>\n"
-					+ "<ToUserName><![CDATA[%s]]></ToUserName>\n"
-					+ "<FromUserName><![CDATA[%s]]></FromUserName>\n"
-					+ "<CreateTime>%s</CreateTime>\n"
-					+ "<MsgType><![CDATA[text]]></MsgType>\n"
-					+ "<Content><![CDATA[%s]]></Content>\n"
-					+ "<FuncFlag>0</FuncFlag>\n" + "</xml>",
-					((Text) message).getFromUserName(),
-					((Text) message).getToUserName(),
-					((Text) message).getCreateTime(), "Hello World!");
-			System.out.println(message.getToUserName());
-			return response;
+		// if (((Text) message).getMsgType().equals(WeixinMessage.TEXT)) {
+		// String response = String.format("<xml>\n"
+		// + "<ToUserName><![CDATA[%s]]></ToUserName>\n"
+		// + "<FromUserName><![CDATA[%s]]></FromUserName>\n"
+		// + "<CreateTime>%s</CreateTime>\n"
+		// + "<MsgType><![CDATA[text]]></MsgType>\n"
+		// + "<Content><![CDATA[%s]]></Content>\n"
+		// + "<FuncFlag>0</FuncFlag>\n" + "</xml>",
+		// ((Text) message).getFromUserName(),
+		// ((Text) message).getToUserName(),
+		// ((Text) message).getCreateTime(), "Hello World!");
+		// System.out.println(message.getToUserName());
+		// return response;
+		// }
+
+		try {
+			XMLEventFactory xmlEventFactory = XMLEventFactory.newInstance();
+			XMLOutputFactory outputFactory = XMLOutputFactory.newInstance();
+			XMLEventWriter writer = outputFactory.createXMLEventWriter(out);
+
+			StartDocument startDocument = xmlEventFactory.createStartDocument(
+					"UTF-8", "1.0");
+			writer.add(startDocument);
+			
+			StartElement rootStartElement = xmlEventFactory.createStartElement("", "", "xml");
+
+			StartElement toUserNameStartElement = xmlEventFactory
+					.createStartElement("", "", "ToUserName");
+			Characters toUserNameChar = xmlEventFactory.createCData(message
+					.getFromUserName());
+			EndElement toUserNameEndElement = xmlEventFactory.createEndElement(
+					"", "", "ToUserName");
+
+			StartElement fromUserNameStartElement = xmlEventFactory
+					.createStartElement("", "", "FromUserName");
+			Characters fromUserNameChar = xmlEventFactory.createCData(message
+					.getToUserName());
+			EndElement fromUserNameEndElement = xmlEventFactory
+					.createEndElement("", "", "FromUserName");
+
+			StartElement createTimeStartElement = xmlEventFactory
+					.createStartElement("", "", "CreateTime");
+			Characters createTimeChar = xmlEventFactory.createCData(message
+					.getCreateTime());
+			EndElement createTimeEndElement = xmlEventFactory.createEndElement(
+					"", "", "CreateTime");
+
+			StartElement msgTypeStartElement = xmlEventFactory
+					.createStartElement("", "", "MsgType");
+			Characters msgTypeChar = xmlEventFactory.createCData(message
+					.getMsgType());
+			EndElement msgTypeEndElement = xmlEventFactory.createEndElement("",
+					"", "Type");
+
+			writer.add(rootStartElement);
+			
+			writer.add(toUserNameStartElement);
+			writer.add(toUserNameChar);
+			writer.add(toUserNameEndElement);
+
+			writer.add(fromUserNameStartElement);
+			writer.add(fromUserNameChar);
+			writer.add(fromUserNameEndElement);
+
+			writer.add(createTimeStartElement);
+			writer.add(createTimeChar);
+			writer.add(createTimeEndElement);
+
+			writer.add(msgTypeStartElement);
+			writer.add(msgTypeChar);
+			writer.add(msgTypeEndElement);
+
+			if (message.getMsgType().equals(WeixinMessage.TEXT)) {
+
+				StartElement contentStartElement = xmlEventFactory
+						.createStartElement("", "", "Content");
+				Characters contentChar = xmlEventFactory
+						.createCData(((Text) message).getContent());
+				EndElement contentEndElement = xmlEventFactory
+						.createEndElement("", "", "Content");
+
+				StartElement funcFlagStartElement = xmlEventFactory
+						.createStartElement("", "", "funcFlagStartElement");
+				Characters funcFlagChar = xmlEventFactory.createCharacters("0");
+				EndElement funcFlagEndElement = xmlEventFactory.createEndElement("", "", "FuncFlag");
+
+				writer.add(contentStartElement);
+				writer.add(contentChar);
+				writer.add(contentEndElement);
+
+				writer.add(funcFlagStartElement);
+				writer.add(funcFlagChar);
+				writer.add(funcFlagEndElement);
+			}
+			
+			EndElement rootEndElement = xmlEventFactory.createEndElement("", "", "xml");
+			writer.add(rootEndElement);
+			
+			EndDocument endDocument = xmlEventFactory.createEndDocument();
+			writer.add(endDocument);
+			
+			writer.flush();
+			writer.close();
+
+		} catch (XMLStreamException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		return null;
 
 	}
 
