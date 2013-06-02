d00e85a5677996dab831918a9e6a4eb8b2b053c5
Louis Hong <www.louishong.com@me.com>
2013-06-02 21:04:25 +0800

    Added a response Database, and changed weixinserver
    UNFINISHED Commit

--- src/com/louishong/ibg/weixin/TestWeixinShift.java
+++ src/com/louishong/ibg/weixin/TestWeixinShift.java
@@ -1,12 +1,8 @@
 package com.louishong.ibg.weixin;
 
 import java.sql.SQLException;
-import java.text.DateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Iterator;
 
-import org.joda.time.LocalDate;
+import com.louishong.database.WeixinShiftWrapper;
 
 public class TestWeixinShift {
 
